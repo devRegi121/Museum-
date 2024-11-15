83ec2df768115f5d7ba14e5b980858e005a36dd3
Megi Muçi <134079375+scarletsk1@users.noreply.github.com>
2024-11-15 21:30:28 +0100

    Add files via upload

---
+++ Museum--main/index.js
@@ -0,0 +1,17 @@
+// Mobile Menu Toggle
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
